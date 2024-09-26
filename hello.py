b63e350699a399355b7ae2b223fec611478feb26
W <111629334+wth-d@users.noreply.github.com>
2024-09-26 10:48:11 -0400

    a1.3 part - up to chap3 end of "custom error pages"

--- hello.py
+++ hello.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
-    return '<h1>Hello world!</h1>'
-
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    # return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
