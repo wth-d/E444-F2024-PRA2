4d179b781a5fc6ccb53cbe58954bca2b45e24824
W <111629334+wth-d@users.noreply.github.com>
2024-09-26 16:39:36 -0400

    a1.4 part - ex4-7

--- hello.py
+++ hello.py
@@ -1,13 +1,22 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+# web forms
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string' # for wtf web form
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -17,11 +26,22 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    # name = None
+    form = NameForm()
+    if form.validate_on_submit(): # when form is submitted AND validated
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data # store it in session dict instead of in a plain Python variable;
+        # form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
     # return '<h1>Hello, {}!</h1>'.format(name)
     return render_template('user.html', name=name)
+
+# print("app.config is", app.config)
